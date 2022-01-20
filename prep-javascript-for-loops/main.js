c306f3e98c62f0165e33af0584a7dbb61d8154dc
Anna Park <parkannuh@gmail.com>
2022-01-20 11:32:29 -0800

    Completed prep-javascript-for-loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let i = -1; i < 9; i++) {
+  var total = i + (i + 2);
+  console.log(total);
+}
+
+for (let i = 100; i >= 1; i--) {
+  console.log('Time till explosion ' + i + '!');
+}
